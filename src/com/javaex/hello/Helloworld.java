ef81fb9fd703c39e180a2030604de395fb92e221
hyunjin-lee-2002 <hyunjinlee2002@gmail.com>
2025-04-17 10:10:35 +0900

    ex01

---
+++ src/com/javaex/hello/Helloworld.java
@@ -0,0 +1,21 @@
+/*
+작성자: 이현진
+작성일: 2025-04-14
+설명: Hello World 출력하기
+*/
+
+package com.javaex.hello;
+
+public class Helloworld {
+	public static void main(String[] args){
+		
+		//문자열을 화면에 출력하는 함수
+        System.out.println("Hello World!");
+        
+        
+        // test git version
+    }
+	
+	
+}
+
